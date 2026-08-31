b9f2572007f11c4fa3423225a685231eadf75b1d
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2026-08-31 22:12:10 +0200

    add CountDownLatch for RaceConditionExampleTest with 10 Threads

--- src/test/java/com/monireh/concurrency/racecondition/RaceConditionExampleTest.java
+++ src/test/java/com/monireh/concurrency/racecondition/RaceConditionExampleTest.java
@@ -1,5 +1,6 @@
 package com.monireh.concurrency.racecondition;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
@@ -8,68 +9,124 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RaceConditionExampleTest {
 
-    @Test
+    @RepeatedTest(10)//@Test
     void shouldDemonstrateRaceCondition()
             throws InterruptedException{
         RaceConditionExample counter = new RaceConditionExample();
 
+
+
+        //-------- with CountDownLatch for 10 Thread --------
+        int numberOfThreads = 10;
         int incrementsPerThread = 100_000;
 
-        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch startLatch =
+                new CountDownLatch(1);
 
-        CountDownLatch doneLatch = new CountDownLatch(2);
+        CountDownLatch doneLatch =
+                new CountDownLatch(numberOfThreads);
 
-        Thread thread1 = new Thread(() -> {
 
-            try {
-                startLatch.await();
 
-                for (int i = 0; i < incrementsPerThread; i++) {
-                    counter.increment();
+        for (int i = 0; i < numberOfThreads; i++) {
+
+            Thread thread = new Thread(() -> {
+
+                try {
+                    startLatch.await();
+
+                    for (int j = 0;
+                         j < incrementsPerThread;
+                         j++) {
+
+                        counter.increment();
+                    }
+
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+
+                } finally {
+                    doneLatch.countDown();
                 }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+            });
 
-            } finally {
-                doneLatch.countDown();
-            }
+            thread.start();
+        }
 
-        });
-
-        Thread thread2 = new Thread(() -> {
-
-            try {
-                startLatch.await();
-
-                for (int i = 0; i < incrementsPerThread; i++) {
-                    counter.increment();
-                }
-
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-
-            } finally {
-                doneLatch.countDown();
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        // Release both threads
+        // Release all threads
         startLatch.countDown();
 
-        // Wait until both threads finish
+        // Wait until all threads finish
         doneLatch.await();
+
+        int expected =
+                numberOfThreads * incrementsPerThread;
 
         int actual = counter.getCount();
 
-        System.out.println("Expected: 200000");
-        System.out.println("Actual:   " + actual);
-
-        assertTrue(
-                actual <= 200_000,
-                "Counter cannot exceed expected value"
+        System.out.println(
+                "Expected: " + expected +
+                        " | Actual: " + actual
         );
+
+        assertTrue(actual <= expected);
+        //-------- with CountDownLatch --------
+//        int incrementsPerThread = 100_000;
+//        CountDownLatch startLatch = new CountDownLatch(1);
+//
+//        CountDownLatch doneLatch = new CountDownLatch(2);
+//
+//        Thread thread1 = new Thread(() -> {
+//
+//            try {
+//                startLatch.await();
+//
+//                for (int i = 0; i < incrementsPerThread; i++) {
+//                    counter.increment();
+//                }
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//
+//            } finally {
+//                doneLatch.countDown();
+//            }
+//
+//        });
+//
+//        Thread thread2 = new Thread(() -> {
+//
+//            try {
+//                startLatch.await();
+//
+//                for (int i = 0; i < incrementsPerThread; i++) {
+//                    counter.increment();
+//                }
+//
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//
+//            } finally {
+//                doneLatch.countDown();
+//            }
+//        });
+//
+//        thread1.start();
+//        thread2.start();
+//
+//        // Release both threads
+//        startLatch.countDown();
+//
+//        // Wait until both threads finish
+//        doneLatch.await();
+//
+//        int actual = counter.getCount();
+//
+//        System.out.println("Expected: 200000");
+//        System.out.println("Actual:   " + actual);
+//
+//        assertTrue(
+//                actual <= 200_000,
+//                "Counter cannot exceed expected value"
+//        );
     }
 }
