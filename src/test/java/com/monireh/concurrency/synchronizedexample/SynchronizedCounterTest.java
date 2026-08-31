0a63b87235dce75d43554a71a6acaf70821f7eee
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2026-08-31 21:48:40 +0200

    add CountDownLatch for SynchronizedCounterTest with 10 Threads

--- src/test/java/com/monireh/concurrency/synchronizedexample/SynchronizedCounterTest.java
+++ src/test/java/com/monireh/concurrency/synchronizedexample/SynchronizedCounterTest.java
@@ -14,49 +14,90 @@ public class SynchronizedCounterTest {
             throws InterruptedException{
         SynchronizedCounter counter = new SynchronizedCounter();
 
+
+
+        //-------- with CountDownLatch for 10 Thread --------
+        int numberOfThreads = 10;
+        int incrementsPerThread = 100_000;
+
         CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch doneLatch = new CountDownLatch(numberOfThreads);
 
-        CountDownLatch doneLatch = new CountDownLatch(2);
+        for(int i = 0; i < numberOfThreads; i++){
+            Thread thread = new Thread(() -> {
 
-        //-------- with CountDownLatch --------
-        Thread thread1 = new Thread(() -> {
-            try{
-                startLatch.await();
+                try {
+                    startLatch.await();
 
-                for (int i = 0; i < 100_000; i++) {
-                    counter.increment();
+                    for (int j = 0; j< incrementsPerThread; j++){
+                        counter.increment();
+                    }
+                } catch (InterruptedException e){
+                    Thread.currentThread().start();
+                } finally {
+                    doneLatch.countDown();
                 }
+            });
 
-            } catch (InterruptedException e){
-                Thread.currentThread().interrupt();
-            } finally {
-                doneLatch.countDown();
-            }
-        });
+            thread.start();
+        }
 
-        Thread thread2 = new Thread(() -> {
-            try{
-                startLatch.await();
-
-                for (int i = 0; i < 100_000; i++) {
-                    counter.increment();
-                }
-
-            } catch (InterruptedException e){
-                Thread.currentThread().interrupt();
-            } finally {
-                doneLatch.countDown();
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        //Release both threads
+        // Release all worker threads
         startLatch.countDown();
 
-        // Wait until both threads finish
+        // Wait until all workers finish
         doneLatch.await();
+        int expected =
+                numberOfThreads * incrementsPerThread;
+
+
+        assertEquals(expected, counter.getCount());
+
+        //-------- with CountDownLatch --------
+//        CountDownLatch startLatch = new CountDownLatch(1);
+//
+//        CountDownLatch doneLatch = new CountDownLatch(2);
+//
+//        Thread thread1 = new Thread(() -> {
+//            try{
+//                startLatch.await();
+//
+//                for (int i = 0; i < 100_000; i++) {
+//                    counter.increment();
+//                }
+//
+//            } catch (InterruptedException e){
+//                Thread.currentThread().interrupt();
+//            } finally {
+//                doneLatch.countDown();
+//            }
+//        });
+//
+//        Thread thread2 = new Thread(() -> {
+//            try{
+//                startLatch.await();
+//
+//                for (int i = 0; i < 100_000; i++) {
+//                    counter.increment();
+//                }
+//
+//            } catch (InterruptedException e){
+//                Thread.currentThread().interrupt();
+//            } finally {
+//                doneLatch.countDown();
+//            }
+//        });
+//
+//        thread1.start();
+//        thread2.start();
+//
+//        //Release both threads
+//        startLatch.countDown();
+//
+//        // Wait until both threads finish
+//        doneLatch.await();
+
+        //-------- without CountDownLatch --------
 
 //        Thread thread1 = new Thread(() -> {
 //            for (int i=0; i<100_000; i++){
@@ -76,6 +117,6 @@ public class SynchronizedCounterTest {
 //        thread1.join();
 //        thread2.join();
 
-        assertEquals(200_000, counter.getCount());
+//        assertEquals(200_000, counter.getCount());
     }
 }
