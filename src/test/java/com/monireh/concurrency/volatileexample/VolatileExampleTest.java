82ae3b1d43ffeb025f06555311b808b30eba2f09
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2026-08-31 22:32:37 +0200

    add CountDownLatch for VolatileExampleTest with 10 Threads

--- src/test/java/com/monireh/concurrency/volatileexample/VolatileExampleTest.java
+++ src/test/java/com/monireh/concurrency/volatileexample/VolatileExampleTest.java
@@ -2,6 +2,8 @@ package com.monireh.concurrency.volatileexample;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class VolatileExampleTest {
@@ -10,17 +12,41 @@ public class VolatileExampleTest {
     void shouldStopWorkerWhenRunningIsSetToFalse() throws InterruptedException{
         VolatileExample worker = new VolatileExample();
 
-        Thread workerThread =
-                new Thread(worker::run);
+        //-------- with CountDownLatch --------
+        CountDownLatch startedLatch =
+                new CountDownLatch(1);
+
+        Thread workerThread = new Thread(() -> {
+
+            startedLatch.countDown();
+
+            worker.run();
+        });
 
         workerThread.start();
 
-        Thread.sleep(100);
+        // Wait until the worker thread has started
+        startedLatch.await();
 
         worker.stop();
 
         workerThread.join(1000);
 
+        assertFalse(workerThread.isAlive());
         assertFalse(worker.isRunning());
+
+        //-------- without CountDownLatch --------
+//        Thread workerThread =
+//                new Thread(worker::run);
+//
+//        workerThread.start();
+//
+//        Thread.sleep(100);
+//
+//        worker.stop();
+//
+//        workerThread.join(1000);
+//
+//        assertFalse(worker.isRunning());
     }
 }
